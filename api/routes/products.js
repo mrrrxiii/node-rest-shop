c652b676ae65917d222ed5f299ef2ce1e71a38b4
mrrrxiii <mrrrxiii@hotmail.com>
2020-06-06 17:13:35 -0400

    first commit before adding db

---
+++ api/routes/products.js
@@ -0,0 +1,58 @@
+const express = require('express')
+const router = express.Router()
+
+router.get('/', (req, res, next) => {
+  res.status(200).json({
+    message: 'Handling GET requests to /products'
+  })
+})
+
+router.get('/', (req, res, next) => {
+  res.status(200).json({
+    message: 'Handling GET requests to /products'
+  })
+})
+
+router.post('/', (req, res, next) => {
+  const product = {
+    name: req.body.name,
+    price: req.body.price
+  }
+  res.status(201).json({
+    message: 'Handling POST requests to /products',
+    createdProduct: product
+  })
+})
+
+router.get('/:productId', (req, res, next) => {
+  const id = req.params.productId
+  if (id === 'special') {
+    res.status(200).json({
+      message: 'You discovered the special ID',
+      id: id
+    })
+  } else {
+    res.status(201).json({
+      message: 'You passed an ID',
+      id: id
+    })
+  }
+})
+
+router.patch('/:productId', (req, res, next) => {
+  const id = req.params.productId
+
+  res.status(200).json({
+    message: 'Update ' + id
+  })
+})
+
+router.delete('/:productId', (req, res, next) => {
+  const id = req.params.productId
+
+  res.status(200).json({
+    message: 'Delete ' + id
+  })
+})
+
+module.exports = router
