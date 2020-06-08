99f52449de669853440fad2550841a291d1e7163
mrrrxiii <mrrrxiii@hotmail.com>
2020-06-07 22:31:57 -0400

    populate product in orders

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose')
 const Order = require('../models/order')
 const Product = require('../models/product')
 router.get('/', (req, res, next) => {
-  Order.find().select('product quantity_id').exec()
+  Order.find().select('product quantity _id').populate('product', 'name').exec()
     .then(docs => {
       res.status(200).json({
         count: docs.length,
@@ -66,7 +66,7 @@ router.post('/', (req, res, next) => {
 })
 
 router.get('/:orderId', (req, res, next) => {
-  Order.findById(req.params.orderId).exec().then(order => {
+  Order.findById(req.params.orderId).populate('product').exec().then(order => {
     if (!order) {
       return res.status(404).json({
         message: 'Order not found'
